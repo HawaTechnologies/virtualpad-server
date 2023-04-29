0ecabc3b3a08ffe3061b8cf18cda3d91648a2a14
Luis Masuelli <luis@datagran.io>
2023-04-29 10:51:29 -0500

    Creating the tools to use admin channels.

---
+++ virtualpad/admin.py
@@ -0,0 +1,66 @@
+import contextlib
+import os
+
+
+_FIFO_SERVER_TO_ADMIN = os.path.expanduser("~/.config/Hawa/run/server-to-admin")
+_FIFO_ADMIN_TO_SERVER = os.path.expanduser("~/.config/Hawa/run/admin-to-server")
+
+
+def _clear_channel_fifo_files():
+    """
+    Destroys the pipes.
+    """
+
+    try:
+        os.unlink(_FIFO_SERVER_TO_ADMIN)
+    except:
+        pass
+
+    try:
+        os.unlink(_FIFO_ADMIN_TO_SERVER)
+    except:
+        pass
+
+
+def _create_channel_fifo_files():
+    """
+    Prepares the pipes.
+    """
+
+    os.mkfifo(_FIFO_SERVER_TO_ADMIN, 0x600)
+    os.mkfifo(_FIFO_ADMIN_TO_SERVER, 0x600)
+    return open(_FIFO_SERVER_TO_ADMIN, 'w'), open(_FIFO_ADMIN_TO_SERVER, 'r')
+
+
+@contextlib.contextmanager
+def using_admin_channel():
+    """
+    A context manager to have the admin channel ready.
+    """
+
+    to_admin = None
+    from_admin = None
+    try:
+        # Clears the channel fifo files (perhaps they lingered).
+        _clear_channel_fifo_files()
+
+        # Creates the channel fifo files (perhaps again).
+        to_admin, from_admin = _create_channel_fifo_files()
+
+        # Uses them.
+        yield to_admin, from_admin
+    finally:
+        # Close to_admin. Forgive any exception.
+        try:
+            to_admin.close()
+        except:
+            pass
+
+        # Close from_admin. Forgive any exception.
+        try:
+            from_admin.close()
+        except:
+            pass
+
+        # Again: clears the channels after usage.
+        _clear_channel_fifo_files()
