8322c8e0d90692b004930cc8eb14410ee47e9f8c
Luis Masuelli <luis@datagran.io>
2023-04-29 18:04:18 -0500

    Adding the making of the parent directories.

--- virtualpad/admin.py
+++ virtualpad/admin.py
@@ -5,9 +5,10 @@ from typing.io import IO
 
 
 # These files are created for the target user.
-START_USER = os.getenv('USER') or os.getenv('USERNAME')
-_FIFO_SERVER_TO_ADMIN = f"/home/{START_USER}/.config/Hawa/run/server-to-admin"
-_FIFO_ADMIN_TO_SERVER = f"/home/{START_USER}/.config/Hawa/run/admin-to-server"
+START_USER = os.getenv('FOR_USER') or os.getenv('USER')
+_FIFO_PATH = f"/home/{START_USER}/.config/Hawa/run"
+_FIFO_SERVER_TO_ADMIN = f"{_FIFO_PATH}/server-to-admin"
+_FIFO_ADMIN_TO_SERVER = f"{_FIFO_PATH}/admin-to-server"
 
 
 def _clear_channel_fifo_files():
@@ -31,6 +32,7 @@ def _create_channel_fifo_files():
     Prepares the pipes.
     """
 
+    os.makedirs(_FIFO_PATH, exist_ok=True)
     os.mkfifo(_FIFO_SERVER_TO_ADMIN, 0x600)
     os.mkfifo(_FIFO_ADMIN_TO_SERVER, 0x600)
     os.system(f"chown {START_USER}:{START_USER} /home/pi/.config/Hawa/run/*")
