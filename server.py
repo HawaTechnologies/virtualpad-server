6fe437129c2fd9ac80cd8b71fb3986d29443c1f8
Luis Masuelli <luis@datagran.io>
2023-04-29 17:47:31 -0500

    Adding the entry point.

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-# TODO implement this!
+#!/usr/bin/python
+from virtualpad.server import main
+from virtualpad.admin import using_admin_channel
+
+
+if __name__ == "__main__":
+    with using_admin_channel() as (admin_writer, admin_reader):
+        main(admin_writer, admin_reader, "Hawa-Virtualpad")
+
