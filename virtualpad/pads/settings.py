58f35a870b52b1326aabc84cecb8b356b8dbece1
Luis Masuelli <luis@datagran.io>
2023-05-30 19:44:37 -0500

    Adding the password-checker to the settings file.

--- virtualpad/pads/settings.py
+++ virtualpad/pads/settings.py
@@ -1,6 +1,7 @@
 import os
 import json
 import random
+from .constants import SLOTS_COUNT
 
 
 SETTINGS_PATH = "/etc/Hawa/virtualpad-server.conf"
@@ -21,13 +22,13 @@ def load():
             return json.load(f)
     except OSError:
         settings = {
-            "passwords": [_regenerate_password() for _ in range(8)]
+            "passwords": [_regenerate_password() for _ in range(SLOTS_COUNT)]
         }
         save(settings)
         return settings
 
 
-def save(settings):
+def save(settings: dict):
     """
     Saves the current VirtualPad settings.
     :param settings: The settings to save.
@@ -36,3 +37,14 @@ def save(settings):
     os.makedirs(os.path.dirname(SETTINGS_PATH), 0o700, exist_ok=True)
     with open(SETTINGS_PATH, 'w') as f:
         return json.dump(settings, f)
+
+
+def check_password(index: int, password: str):
+    """
+    Checks the index and the passwords.
+    :param index: The index to check.
+    :param password: The password to check.
+    :return: Whether the index is valid and the password matches.
+    """
+
+    return index in range(SLOTS_COUNT) and load()["passwords"][index] == password
