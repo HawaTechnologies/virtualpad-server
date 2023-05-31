a17533de0680e52db66d2ab0cd5b9d629607b842
Luis Masuelli <luis@datagran.io>
2023-05-30 21:49:02 -0500

    Updating the pad server with the new mechanism.

--- virtualpad/pad_server.py
+++ virtualpad/pad_server.py
@@ -5,10 +5,9 @@ import threading
 import socketserver
 import traceback
 from typing import Any, Type, Tuple
-from virtualpad.base_server import IndexedTCPServer, IndexedHandler, launch_server_in_thread
-from virtualpad.broadcast_server import BroadcastServer
-from virtualpad.pads import MAX_PAD_COUNT, pad_get, pad_send_all, pad_set, pad_clear, PadMismatch, \
-    pad_check_password, PadNotInUse
+from .base_server import IndexedTCPServer, IndexedHandler, launch_server_in_thread
+from .broadcast_server import BroadcastServer
+from .pads import PadSlots, SLOTS_INDICES, PadNotInUse, PadIndexOutOfRange, PadInUse, AuthenticationFailed, PadMismatch
 
 # Logger and settings.
 LOGGER = logging.getLogger("hawa.virtualpad.pad-server")
@@ -35,34 +34,34 @@ CLOSE_CONNECTION = N_BUTTONS + 1
 PING = N_BUTTONS + 2
 
 
-def _pad_auth(remote: socketserver.StreamRequestHandler):
+def _pad_auth(remote: 'PadHandler'):
     """
-    Attempts an authentication.
-    :returns: A tuple (False, None, None) or (True, index, nickname) if success.
+    Reads (receives) and parses a pad auth message.
+    :param remote: The remote to receive it from.
+    :return: The received and parsed data.
     """
 
     # Getting the first data.
     read = remote.rfile.read(22)
     if len(read) < 22:
-        raise Exception("Login handshake incomplete or aborted")
+        raise RuntimeError("Login handshake incomplete or aborted")
 
-    index = read[0]
+    pad_index = read[0]
     attempted = bytes(read[1:5]).decode("utf-8")
     nickname = bytes(read[5:21]).decode("utf-8").rstrip('\b')
-    LOGGER.info(f"For index {index}, '{nickname}' attempts to join")
-    # Authenticating.
-    if index >= MAX_PAD_COUNT:
+    LOGGER.info(f"For pad index {pad_index}, '{nickname}' attempts to join")
+    try:
+        remote.slots.occupy(pad_index, nickname, attempted, remote.index)
+        return pad_index
+    except PadIndexOutOfRange:
         remote.wfile.write(PAD_INVALID)
-        return False, None, None, None
-    entry = pad_get(index)
-    if entry[0]:
+        raise
+    except PadInUse:
         remote.wfile.write(PAD_BUSY)
-        return False, None, None, None
-    if not pad_check_password(index, attempted):
+        raise
+    except AuthenticationFailed:
         remote.wfile.write(LOGIN_FAILURE)
-        return False, None, None, None
-    remote.wfile.write(LOGIN_SUCCESS)
-    return True, index, nickname
+        raise
 
 
 class PadHandler(IndexedHandler):
@@ -71,10 +70,14 @@ class PadHandler(IndexedHandler):
         self._has_ping = False
         self._pad_index = None
         self._nickname = None
-        self._device = None
         if not isinstance(server, PadServer):
             raise ValueError("Only a MainServer (or subclasses) can use a PadHandler")
+        self._slots = server.slots
         super().__init__(request, client_address, server)
+
+    @property
+    def slots(self):
+        return self._slots
 
     def _send(self, obj):
         self.wfile.write(f"{json.dumps(obj)}\n".encode("utf-8"))
@@ -82,26 +85,12 @@ class PadHandler(IndexedHandler):
     def _broadcast(self, obj):
         self.server.broadcast(f"{json.dumps(obj)}\n".encode("utf-8"))
 
-    def _auth(self) -> None:
-        """
-        Attempts an authentication.
-        """
-
-        LOGGER.info(f"Remote #{self.index} Logging in")
-        success, index, nickname = _pad_auth(self)
-        if not success:
-            LOGGER.info(f"Remote #{self.index} failed to log in")
-        else:
-            LOGGER.info(f"Remote #{self.index} successfully logged in")
-            self._pad_index = index
-            self._nickname = nickname
-
     def _heartbeat(self) -> None:
         """
         Heartbeat for the gamepad.
         """
 
-        while self._device and self._device == pad_get(self._pad_index)[0]:
+        while self._pad_index in SLOTS_INDICES and self._slots[self._pad_index].connection_index == self.index:
             time.sleep(_HEARTBEAT_INTERVAL)
             if self._has_ping:
                 self._has_ping = False
@@ -110,7 +99,7 @@ class PadHandler(IndexedHandler):
                     self._broadcast({"type": "notification", "command": "pad:timeout", "index": self._pad_index})
                     try:
                         self.wfile.write(TIMEOUT)
-                        pad_clear(self._pad_index, self._device)
+                        self._slots.release(self._pad_index, True, self.index, True)
                     except PadNotInUse:
                         pass
                 self._device = None
@@ -118,18 +107,18 @@ class PadHandler(IndexedHandler):
 
     def _init_pad(self) -> None:
         """
-        Inits the pad.
+        Attempts an authentication. On success, it establishes the _pad_index
+        to a value other than None. On failure, it keeps _pad_index == None.
+        The heartbeat is already launched on success.
         """
 
-        # Setting the device.
-        pad_set(self._pad_index, self.server.device_name, self._nickname)
-        self._broadcast({"type": "notification", "command": "pad:set", "nickname": self._nickname,
-                         "index": self._pad_index})
-        entry = pad_get(self._pad_index)
-        device, _ = entry
-        self._device = device
-        # Launching the heartbeat.
-        threading.Thread(target=self._heartbeat).start()
+        LOGGER.info(f"Remote #{self.index} Logging in")
+        try:
+            self._pad_index = _pad_auth(self)
+            threading.Thread(target=self._heartbeat).start()
+            LOGGER.info(f"Remote #{self.index} successfully logged in")
+        except Exception as e:
+            LOGGER.info(f"Remote #{self.index} failed to log in: {type(e).__name__} -> {e}")
 
     def _process_events(self, buffer: bytes):
         """
@@ -154,23 +143,18 @@ class PadHandler(IndexedHandler):
         # Send the data. If the current pad is different,
         # then this thread ends.
         try:
-            pad_send_all(self._pad_index, fixed, self._device)
+            self._slots.emit(self._pad_index, fixed, self.index)
         except:
             traceback.print_exc()
 
     def setup(self) -> None:
         super().setup()
         LOGGER.info(f"Remote #{self.index} starting")
-        self._auth()
-        if self._pad_index is not None:
-            self._init_pad()
-        else:
-            self._device = None
+        self._init_pad()
 
     def handle(self) -> None:
-        device = self._device
         try:
-            while self._device:
+            while self._pad_index is not None:
                 try:
                     buffer = self.rfile.read(1)
                 except ConnectionResetError:
@@ -188,8 +172,8 @@ class PadHandler(IndexedHandler):
                         return
                     self._process_events(commands)
                 elif length == CLOSE_CONNECTION:
-                    pad_clear(self._pad_index)
-                    device = None
+                    self.slots.release(self._pad_index, False, self.index, True)
+                    self._pad_index = None
                     return
                 elif length == PING:
                     LOGGER.info(f"Remote #{self.index} ping")
@@ -203,11 +187,11 @@ class PadHandler(IndexedHandler):
             raise
         finally:
             try:
-                if self._device:
-                    pad_clear(self._pad_index, device)
+                if self._pad_index:
+                    self.slots.release(self._pad_index, False, self.index, False)
             except PadNotInUse:
                 pass
-            self._device = None
+            self._pad_index = None
 
     def finish(self) -> None:
         super().finish()
@@ -220,27 +204,38 @@ class PadServer(IndexedTCPServer):
     """
 
     def __init__(self, server_address: Tuple[str, int], RequestHandlerClass: Type[socketserver.BaseRequestHandler],
-                 bind_and_activate: bool, broadcast_server: IndexedTCPServer, device_name: str = "Hawa-VirtualPad"):
+                 bind_and_activate: bool, broadcast_server: IndexedTCPServer, slots: PadSlots):
         super().__init__(server_address, RequestHandlerClass, bind_and_activate)
-        self._device_name = device_name
+        self._slots = slots
+        self._use_heartbeat_loop = False
         self._broadcast_server = broadcast_server
 
     @property
-    def device_name(self):
-        return self._device_name
+    def slots(self):
+        return self._slots
 
     def broadcast(self, message: bytes):
         self._broadcast_server.broadcast(message)
 
+    def _heartbeat_loop(self):
+        while self._use_heartbeat_loop:
+            result = enumerate(self._slots.heartbeat())
+            for pad_index, device_disposed in result:
+                if device_disposed:
+                    LOGGER.log(f"Pad #{pad_index}'s device disposed on no use")
+            time.sleep(1)
+
     def server_activate(self) -> None:
         super().server_activate()
+        self._use_heartbeat_loop = True
+        threading.Thread(target=self._heartbeat_loop).start()
         LOGGER.info("Server started")
 
     def server_close(self) -> None:
         super().server_close()
+        self._use_heartbeat_loop = False
         LOGGER.info("Server stopped")
 
 
-def launch_pad_server(broadcast_server: BroadcastServer) -> socketserver.TCPServer:
-    return launch_server_in_thread(PadServer, ("0.0.0.0", PAD_PORT), PadHandler, broadcast_server)
-
+def launch_pad_server(broadcast_server: BroadcastServer, slots: PadSlots) -> socketserver.TCPServer:
+    return launch_server_in_thread(PadServer, ("0.0.0.0", PAD_PORT), PadHandler, broadcast_server, slots)
